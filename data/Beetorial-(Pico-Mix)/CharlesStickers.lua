9a3be3791118f7d23d450f7ef8132fda58942599
Axolotlguy13212 <practicleant1321@outlook.com>
2025-09-13 15:48:22 -0500

    Added stickers

---
+++ data/Beetorial-(Pico-Mix)/CharlesStickers.lua
@@ -0,0 +1,8 @@
+
+-- Copy and pasted the code from P-Slice Wiki
+-- I HATE LUA, OKAY
+
+
+
+setPropertyFromClass("mikolka.vslice.StickerSubState", "Charles-Stickers-Set-1", "all")
+changeTransStickers("Charles-Stickers-Set-1","all")
